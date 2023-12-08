49b6b17c8e2e3dbd3afb3cd012070a25a612235c
ChandraRaditya <chandra@refactory.id>
2023-12-08 16:59:40 +0700

    add calculation item

--- src/app/ShopSection/index.tsx
+++ src/app/ShopSection/index.tsx
@@ -4,12 +4,17 @@ import { getData } from "../Services";
 
 export default function ShopSection() {
   const [data, setData] = useState([]);
+  const [item, setItem] = useState(0);
 
   useEffect(() => {
     getData().then((response) => {
       setData(response?.data || []);
     });
   }, []);
+
+    const onChange = (input: any, price: any) => {
+       setItem(input.target.value);
+  };
 
   return (
     <div className="flex flex-col">
@@ -22,30 +27,36 @@ export default function ShopSection() {
       </div>
       {data.map((val: any, index) => {
         return (
-          <div
-            key={index}
-            className="h-[250px] flex flex-row items-center"
-          >
+          <div key={index} className="h-[250px] flex flex-row items-center">
             <img
               className="w-[200px] h-[200px] mx-[30px]"
               src={val.product.media_url}
               alt=""
             />
             <div className="flex flex-col gap-3 grow">
-                    <p className="text-blue-400">{val.product.code}</p>
-                    <p className="text-lg">{val.product.name}</p>
+              <p className="text-blue-400">{val.product.code}</p>
+              <p className="text-lg">{val.product.name}</p>
               <div className="text-xs">
-                        <div>{val.product.price}</div>
-                        <div className="text-red-500">{val.product.stock} In Stock</div>
+                <div>{val.product.price}</div>
+                <div className="text-red-500">{val.product.stock} In Stock</div>
               </div>
             </div>
             <div className="min-w-[400px] flex flex-row justify-evenly">
-              <p className="text-center">10</p>
-              <p>10</p>
+              {/* <p className="text-center">10</p> */}
+              <input
+                className="border-[2px] border-black w-[100px]"
+                type="number"
+                onChange={(input) => onChange(input, val.product.price)}
+              />
+              <p>{item * val.product.price}</p>
             </div>
           </div>
         );
       })}
+      <div className="bg-black h-[100px] flex flex-row-reverse text-white items-center">
+        <p className="min-w-[200px]">Rp. 10000</p>
+        <p className="min-w-[200px]">Total</p>
+      </div>
     </div>
   );
 }
